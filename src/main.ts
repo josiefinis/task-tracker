7aecee099022f2640ab2fb8a150b7debf626f1c4
Josefin Wall <josefin1010wall@gmail.com>
2026-06-16 15:44:29 +0200

    Follow typescript style guide.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 interface Task {
-    taskName: string;
-    priority: 1 | 2 | 3 | 4 | 5;
-    isCompleted: boolean;
+  name: string;
+  priority: 1 | 2 | 3 | 4 | 5;
+  isCompleted: boolean;
 }
 
-const task1: Task = {taskName: "make dinner", priority: 5, isCompleted: false };
-const task2: Task = {taskName: "do laundry", priority: 3, isCompleted: false };
-const task3: Task = {taskName: "water plants", priority: 4, isCompleted: true };
+const task1: Task = {name: "make dinner", priority: 5, isCompleted: false };
+const task2: Task = {name: "do laundry", priority: 3, isCompleted: false };
+const task3: Task = {name: "water plants", priority: 4, isCompleted: true };
 
 const completedTasks: number = 1;
 const totalTasks: number = 3;
@@ -17,9 +17,9 @@ Task Summary
 ------------------------------------------
 \t\tPriority\tTask
 ------------------------------------------
-${task1.isCompleted ? "[x]": "[ ]"}\t\t${task1.priority}\t\t\t${task1.taskName} 
-${task2.isCompleted ? "[x]": "[ ]"}\t\t${task2.priority}\t\t\t${task2.taskName} 
-${task3.isCompleted ? "[x]": "[ ]"}\t\t${task3.priority}\t\t\t${task3.taskName} 
+${task1.isCompleted ? "[x]": "[ ]"}\t\t${task1.priority}\t\t\t${task1.name} 
+${task2.isCompleted ? "[x]": "[ ]"}\t\t${task2.priority}\t\t\t${task2.name} 
+${task3.isCompleted ? "[x]": "[ ]"}\t\t${task3.priority}\t\t\t${task3.name} 
 
 You have completed ${completionRate.toFixed(0)}% of your tasks.
 `);

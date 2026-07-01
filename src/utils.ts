f3c3ea7f308e62616e4d8e9f8e2c2228178cecf6
Josefin Wall <josefin1010wall@gmail.com>
2026-07-01 15:32:12 +0200

    Move rendering and utilities to own modules.

---
+++ src/utils.ts
@@ -0,0 +1,32 @@
+import type { Priority } from "./types";
+
+export function toPriority(value: string | number): Priority {
+  if ([1, 2, 3, 4, 5].includes(+value)) {
+    return value as Priority;
+  } else {
+    throw new Error(`Can not convert ${value} to type Priority.`);
+  }
+}
+
+export function incrementPriority(priority: Priority): Priority {
+  priority %= 5;
+  priority++;
+  return toPriority(priority);
+}
+
+export function validateTaskName(name: string): string {
+  const errorMessage: string = !name
+    ? "Task name is required."
+    : name.length > 30
+      ? "Task name should be no more than 30 characters"
+      : "";
+  return errorMessage;
+}
+
+export function displayLastSavedDate(): void {
+  const lastSaved = localStorage.getItem("lastSaved") ?? Date();
+  const element = document.getElementById("last-saved");
+  if (element) {
+    element.textContent = `Last saved: ${lastSaved}`;
+  }
+}
